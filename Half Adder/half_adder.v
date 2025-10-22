50605dec3193ba67db4324794ab2bc0a87c0a359
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2025-10-22 21:43:02 +0530

    Update half_adder.v

--- Half Adder/half_adder.v
+++ Half Adder/half_adder.v
@@ -1,1 +1,13 @@
+module half_adder(a, b, sum, carry);
 
+//Port declaration
+
+input  a, b;  //input declaration
+output sum, carry;  //output declaration
+  
+//sum and carry expression for half adder
+
+assign sum   =  a ^ b;  //xor operation
+assign carry =  a & b;  //and operation
+  
+endmodule 
