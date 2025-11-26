307c4d1013bfc4d58e4bfa3344e277e686fc42c5
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2025-11-26 18:00:49 +0530

    Update half_subtractor.v

--- Half Subtractor/half_subtractor.v
+++ Half Subtractor/half_subtractor.v
@@ -1,1 +1,10 @@
+module half_subtractor(a, b, sum, carry);
+
+input  a, b;  //input declaration
+output diff, borrow;  //output declaration
+
+assign diff   =  a ^ b;  
+assign borrow =  (~a) & b;  
+  
+endmodule 
 
