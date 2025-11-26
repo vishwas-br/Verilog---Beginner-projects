41e266671dccefedaf1103322ff92c4adc310a01
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2025-11-26 18:03:05 +0530

    Create half_subtractor_tb.v

---
+++ Half Subtractor/half_subtractor_tb.v
@@ -0,0 +1,24 @@
+module half_subtractor_tb;
+    reg a, b;
+    wire diff, borrow;
+
+    half_subtractor uut (
+        .a(a),
+        .b(b),
+        .diff(diff),
+        .borrow(borrow)
+    );
+
+    initial begin
+        $display("a b | diff borrow");
+        $monitor("%b %b |  %b     %b", a, b, diff, borrow);
+
+        // Test all input combinations
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+
+        #10 $finish;
+    end
+endmodule
