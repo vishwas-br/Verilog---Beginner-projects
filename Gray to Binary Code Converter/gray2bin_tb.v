483ab0acb8ee34046f28b8f50ad40e5fbdaec9da
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2025-12-14 15:10:31 +0530

    Create gray2bin_tb.v

---
+++ Gray to Binary Code Converter/gray2bin_tb.v
@@ -0,0 +1,18 @@
+module gray2bin_tb;
+ reg [3:0] binary, gray;
+ gray2bin g2b(gray, binary);
+ 
+ initial begin
+   $monitor("Gray = %b ; Binary = %b", gray, binary);
+ gray = 4'b1110; #1;
+ gray = 4'b0100; #1;
+ gray = 4'b0111; #1;
+ gray = 4'b1010; #1;
+ gray = 4'b1000; #1;
+ gray = 4'b0000; #1;
+ gray = 4'b0100; #1;
+ gray = 4'b0110; #1;
+ gray = 4'b1011; #1;
+ gray = 4'b0000;
+ end
+endmodule
