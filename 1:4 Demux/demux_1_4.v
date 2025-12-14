670a8fd51811b62716c884607bd9466e94f52da6
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2025-12-14 15:43:40 +0530

    Create demux_1_4.v

---
+++ 1:4 Demux/demux_1_4.v
@@ -0,0 +1,15 @@
+module demux_1_4()
+  input [1:0] sel;
+  input in;
+  output y0, y1, y2, y3;
+
+  always @(*) begin
+    case(sel)
+      2'h00: {y0, y1, y2, y3} = {in, 3'b0};
+      2'h01: {y0, y1, y2, y3} = {1'b0, in, 2'b0};
+      2'h10: {y0, y1, y2, y3} = {2'b0, in, 1'b0};
+      2'h11: {y0, y1, y2, y3} = {3'b0, in};
+      default: $display("Invalid sel input");
+    endcase
+  end
+endmodule
