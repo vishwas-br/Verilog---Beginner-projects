66434c71303f8f34f1af99bd7c1e6ae5a181ab32
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2025-12-14 13:04:08 +0530

    Create mux_2_1_tb.v

---
+++ 2:1_MUX/mux_2_1_tb.v
@@ -0,0 +1,17 @@
+module mux_2_1_tb;
+ reg i0, i1, sel;
+ wire y;
+ 
+ mux_2_1 mux(sel, i0, i1, y);
+ initial begin
+ $monitor("sel = %h: i0 = %h, i1 = %h --> y = %h", sel, i0, i1, y);
+ i0 = 0; i1 = 1;
+ sel = 0;
+ #2;
+ sel = 1;
+ #2;
+ sel = 1;
+ #2;
+ sel = 0;
+ end
+endmodule
