9b918a916313b75166158790d3b07d3c55419da6
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2026-01-13 14:53:25 +0530

    Update gray2bin.v

--- Gray to Binary Code Converter/gray2bin.v
+++ Gray to Binary Code Converter/gray2bin.v
@@ -2,7 +2,7 @@ module gray2bin #(parameter WIDTH=4) (input [WIDTH-1:0] gray, output [WIDTH-1:0]
 
  genvar i;
  generate
-   for(i = 0; i < WIDTH; i++) begin
+  for(i = 0; i < WIDTH; i = i + 1) begin
      assign binary[i] = ^(gray >> i);
    end
  endgenerate
