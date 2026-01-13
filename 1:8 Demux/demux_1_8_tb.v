0a6a3f659bd5e834fa8b0da4ea005b261395ff27
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2026-01-13 15:31:48 +0530

    Update demux_1_8_tb.v

--- 1:8 Demux/demux_1_8_tb.v
+++ 1:8 Demux/demux_1_8_tb.v
@@ -6,22 +6,22 @@ module demux_1_8_tb;
  demux_1_8 demux(sel, in, y0, y1, y2, y3, y4, y5, y6, y7);
  
  initial begin
-   $monitor("sel = %b, in = %b, y0 = %0b, y1 = %0b, y2 = %0b, y3 = %0b, y4 = %0b, y5 = %0b, y6 = %0b, y7 = %0b", sel, i, y0, y1, y2, y3, y4, y5, y6, y7);
-    sel = 3'b000; in = 0; #1;
-    sel = 3'b000; in = 1; #1;
-    sel = 3'b001; in = 0; #1;
-    sel = 3'b001; in = 1; #1;
-    sel = 3'b010; in = 0; #1;
-    sel = 3'b010; in = 1; #1;
-    sel = 3'b011; in = 0; #1;
-    sel = 3'b011; in = 1; #1;
-    sel = 3'b100; in = 0; #1;
-    sel = 3'b100; in = 1; #1;
-    sel = 3'b101; in = 0; #1;
-    sel = 3'b101; in = 1; #1;
-    sel = 3'b110; in = 0; #1;
-    sel = 3'b110; in = 1; #1;
-    sel = 3'b111; in = 0; #1;
-    sel = 3'b111; in = 1; #1;
+  $monitor("sel = %b, in = %b, y0 = %0b, y1 = %0b, y2 = %0b, y3 = %0b, y4 = %0b, y5 = %0b, y6 = %0b, y7 = %0b", sel, in, y0, y1, y2, y3, y4, y5, y6, y7);
+    sel = 3'b000; in = 0; #5;
+    sel = 3'b000; in = 1; #5;
+    sel = 3'b001; in = 0; #5;
+    sel = 3'b001; in = 1; #5;
+    sel = 3'b010; in = 0; #5;
+    sel = 3'b010; in = 1; #5;
+    sel = 3'b011; in = 0; #5;
+    sel = 3'b011; in = 1; #5;
+    sel = 3'b100; in = 0; #5;
+    sel = 3'b100; in = 1; #5;
+    sel = 3'b101; in = 0; #5;
+    sel = 3'b101; in = 1; #5;
+    sel = 3'b110; in = 0; #5;
+    sel = 3'b110; in = 1; #5;
+    sel = 3'b111; in = 0; #5;
+    sel = 3'b111; in = 1; #5;
   end
 endmodule
