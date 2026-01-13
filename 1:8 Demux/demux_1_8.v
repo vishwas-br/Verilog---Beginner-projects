5a497ce9bd9665ed75fb08126dfe1591c0fe1586
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2026-01-13 15:03:46 +0530

    Update demux_1_8.v

--- 1:8 Demux/demux_1_8.v
+++ 1:8 Demux/demux_1_8.v
@@ -1,18 +1,18 @@
 module demux_1_8()
   input [2:0] sel;
   input in;
-  output y0, y1, y2, y3, y4, y5, y6, y7, y8;
+  output y0, y1, y2, y3, y4, y5, y6, y7;
 
   always @(*) begin
     case(sel)
-      3'h000: {y0, y1, y2, y3, y4, y5, y6, y7, y8} = {in, 7'b0};
-      3'h001: {y0, y1, y2, y3, y4, y5, y6, y7, y8} = {1'b0, in, 6'b0};
-      3'h010: {y0, y1, y2, y3, y4, y5, y6, y7, y8} = {2'b0, in, 5'b0};
-      3'h011: {y0, y1, y2, y3, y4, y5, y6, y7, y8} = {3'b0, in, 4'b0};
-      3'h100: {y0, y1, y2, y3, y4, y5, y6, y7, y8} = {4'b0, in, 3'b0};
-      3'h101: {y0, y1, y2, y3, y4, y5, y6, y7, y8} = {5'b0, in, 2'b0};
-      3'h110: {y0, y1, y2, y3, y4, y5, y6, y7, y8} = {6'b0, in, 1'b0};
-      3'h111: {y0, y1, y2, y3, y4, y5, y6, y7, y8} = { 7'b0, in};
+      3'h000: {y0, y1, y2, y3, y4, y5, y6, y7} = {in, 7'b0};
+      3'h001: {y0, y1, y2, y3, y4, y5, y6, y7} = {1'b0, in, 6'b0};
+      3'h010: {y0, y1, y2, y3, y4, y5, y6, y7} = {2'b0, in, 5'b0};
+      3'h011: {y0, y1, y2, y3, y4, y5, y6, y7} = {3'b0, in, 4'b0};
+      3'h100: {y0, y1, y2, y3, y4, y5, y6, y7} = {4'b0, in, 3'b0};
+      3'h101: {y0, y1, y2, y3, y4, y5, y6, y7} = {5'b0, in, 2'b0};
+      3'h110: {y0, y1, y2, y3, y4, y5, y6, y7} = {6'b0, in, 1'b0};
+      3'h111: {y0, y1, y2, y3, y4, y5, y6, y7} = { 7'b0, in};
       default: $display("Invalid sel input");
     endcase
   end
