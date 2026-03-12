db893a62857aa07cd8adaad1fe10559ca22421f8
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2026-03-12 15:45:32 +0530

    Update demux_1_8_tb.v

--- 1:8 Demux/demux_1_8_tb.v
+++ 1:8 Demux/demux_1_8_tb.v
@@ -1,9 +1,11 @@
+`timescale 1ns/1ps
+
 module demux_1_8_tb;
  reg [2:0] sel;
  reg in;
  wire y0, y1, y2, y3, y4, y5, y6, y7;
  
- demux_1_8 demux(sel, in, y0, y1, y2, y3, y4, y5, y6, y7);
+ demux_1_8 uut(.sel(sel), .in(in), .y0(y0), .y1(y1), .y2(y2), .y3(y3), .y4(y4), .y5(y5), .y6(y6), .y7(y7));
  
  initial begin
   $monitor("sel = %b, in = %b, y0 = %0b, y1 = %0b, y2 = %0b, y3 = %0b, y4 = %0b, y5 = %0b, y6 = %0b, y7 = %0b", sel, in, y0, y1, y2, y3, y4, y5, y6, y7);
