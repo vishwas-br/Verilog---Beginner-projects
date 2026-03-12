1d4b7f6ec14c6a18536e607b6f2853321efb54a8
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2026-03-12 14:29:28 +0530

    Update demux_1_2.v

--- 1:2 Demux/demux_1_2.v
+++ 1:2 Demux/demux_1_2.v
@@ -1,7 +1,7 @@
-module demux_1_2()
-  input sel;
-  input in;
-  output y0, y1;
+module demux_1_2(
+  input sel,
+  input in,
+  output y0, y1);
   
   assign {y0, y1} = sel ? { 1'b0, in} : {in, 1'b0};
 endmodule
