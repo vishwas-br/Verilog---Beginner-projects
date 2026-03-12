b47d3da622c10d740399375e91e49106fe42b9a1
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2026-03-12 15:09:55 +0530

    Update demux_1_2.v

--- 1:2 Demux/demux_1_2.v
+++ 1:2 Demux/demux_1_2.v
@@ -1,14 +1,15 @@
 module demux_1_2(
   input sel,
   input in,
-  output y0, y1);
-  
-  alwaya @(*) begin
-     y0 = 0;
-     y1 = 0;
-     if (sel == 0)
-        y0 = in;
-    ]else
-        y1 = in;
+  output reg y0, 
+  output reg y1);
+  initial begin
+    y0 = 0;
+    y1 = 0;
+  end
+
+  assign y1 = sel & in;
+  assign y0 = ~sel & in;
+
   end
 endmodule
