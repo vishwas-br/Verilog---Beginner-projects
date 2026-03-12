36778fb2f2969f101406701a6a041108f4896793
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2026-03-12 15:10:55 +0530

    Update demux_1_2_tb.v

--- 1:2 Demux/demux_1_2_tb.v
+++ 1:2 Demux/demux_1_2_tb.v
@@ -1,13 +1,15 @@
+`timescale 1ns/1ps
+
 module demux_1_2_tb;
  reg sel, in;
  wire y0, y1;
  
-   demux_1_2 demux(sel, in, y0, y1);
+   demux_1_2 uut(sel, in, y0, y1);
    initial begin
      $monitor("sel = %h: in = %h ; y0 = %h, y1 = %h", sel, in, y0, y1);
-     sel=0; in=0; #1;
-     sel=0; in=1; #1;
-     sel=1; in=0; #1;
-     sel=1; in=1; #1;
+     sel=0; in=0; #50;
+     sel=0; in=1; #50;
+     sel=1; in=0; #50;
+     sel=1; in=1; #50;
    end
 endmodule
