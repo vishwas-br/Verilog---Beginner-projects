a8873bc05a7816dc7c7e7a9c75f0d748da77d92a
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2026-03-26 12:02:08 +0530

    Update full_subtractor_tb.v

--- Full Subtractor/full_subtractor_tb.v
+++ Full Subtractor/full_subtractor_tb.v
@@ -1,27 +1,27 @@
 module full_subtractor_tb;
-reg a, b, bin;
-wire diff, borrow;
+  reg a, b, bin;
+  wire diff, borrow;
 
   full_subtractor uut(a, b, bin, diff, borrow);
 
-initial begin
-a = 0; b = 0; bin = 0;
-#10
-a = 0; b = 0; bin = 1;
-#10
-a = 0; b = 1; bin = 0;
-#10
-a = 0; b = 1; bin = 1;
-#10
-a = 1; b = 0; bin = 0;
-#10
-a = 1; b = 0; bin = 1;
-#10
-a = 1; b = 1; bin = 0;
-#10
-a = 1; b = 1; bin = 1;
-#10
-$finish();
-end
+  initial begin
+    a = 0; b = 0; bin = 0;
+    #10
+    a = 0; b = 0; bin = 1;
+    #10
+    a = 0; b = 1; bin = 0;
+    #10
+    a = 0; b = 1; bin = 1;
+    #10
+    a = 1; b = 0; bin = 0;
+    #10
+    a = 1; b = 0; bin = 1;
+    #10
+    a = 1; b = 1; bin = 0;
+    #10
+    a = 1; b = 1; bin = 1;
+    #10
+    $finish;
+  end
                 
 endmodule
